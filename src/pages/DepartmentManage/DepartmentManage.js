4d19cc13700ce03cf7221ae46bab0c0c3daa3e5e
foreLink_\k <so191925@gmail.com>
2023-05-30 10:22:10 +0900

    style: 전역 Header 컴포넌트로 분리

--- src/pages/DepartmentManage/DepartmentManage.js
+++ src/pages/DepartmentManage/DepartmentManage.js
@@ -1,56 +1,14 @@
 import styled from "styled-components";
 import SideNav from "../../components/SideNav/SideNav";
-import {FaUserCircle} from 'react-icons/fa';
-import {BsArrowBarRight} from 'react-icons/bs';
-// import { CompanyDummy, companyColumnData } from "./CompanyDummy";
 import { useState } from "react";
-// import { useTable } from "react-table";
+import { Header } from "../../components";
 import { DepartmentTable } from "../../components/Table/DepartmentTable";
 
-
-const SHeader = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 60px;
-  background-color: white;
-  color:  ${({theme}) => theme.colors.blue090};
-  font-size: 1.1em;
-  font-weight: 500;
-  box-shadow: 0 2px 18px -2px rgba(0, 0, 0, 0.5);
-
-`
 
 const SWrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-`
-
-const SCompanyWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  min-width: 45%;
-  padding: 0px 30px;
-  gap: 40px;
-  font-size: 1.3em;
-  font-weight: 800;
-`
-
-const SUserWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  width: 100%;
-  justify-content: flex-end;
-  padding: 0px 30px;
-  gap: 20px;
-`
-
-const SUserInfo = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 5px;
 `
 
 const SContentWrapper = styled.div`
@@ -160,20 +118,7 @@ const DepartmentManage = () => {
 
   return (
   <SWrapper>
-    <SHeader>
-      <SCompanyWrapper>
-        <div>Payroll Auto</div>
-        <BsArrowBarRight size={25}/>
-      </SCompanyWrapper>
-      <SUserWrapper>
-        <div>계약 만료일 : 2025-12-31</div>
-        <SUserInfo>
-          <FaUserCircle size={32}/>
-          <div>Username</div>
-        </SUserInfo>
-        <div>로그아웃</div>
-      </SUserWrapper>
-    </SHeader>
+    <Header />
     <SContentWrapper>
       <SideNav />
       <SContentContainer>
